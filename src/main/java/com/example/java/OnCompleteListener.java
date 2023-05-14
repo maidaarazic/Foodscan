7a964b60bdf5aa5cd7050cd50cc7857e6bd781d1
maidaarazic <75145162+maidaarazic@users.noreply.github.com>
2023-05-14 21:48:47 +0200

    Delete src/main directory

--- src/main/java/com/example/java/OnCompleteListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.registration;
-
-import androidx.annotation.NonNull;
-
-
-public abstract class OnCompleteListener<T> {
-    public abstract void onComplete(@NonNull com.example.registration.Task<Void> task);
-}
--- src/main/java/com/example/java/Task.java
+++
@@ -1,7 +0,0 @@
-package com.example.registration;
-
-public class Task<T> {
-    public boolean isSuccessful() {
-        return false;
-    }
-}
